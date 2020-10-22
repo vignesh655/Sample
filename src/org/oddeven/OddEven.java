9719d78ea17fbc8d4d4a96ea9a304ac8c6cac511
vignesh655 <vignesh655@gmail.com>
2020-10-22 12:40:02 +0530

    completed successfully

---
+++ src/org/oddeven/OddEven.java
@@ -0,0 +1,19 @@
+package org.oddeven;
+
+import java.util.Scanner;
+
+public class OddEven {
+	public static void main(String[] args) {
+		int num;
+		System.out.println("Enter a number");
+		Scanner sc=new Scanner(System.in);
+	    num= sc.nextInt();
+		if (num%2==0) {
+			System.out.println("Even number");
+		} else {
+			System.out.println("odd number");
+
+		}
+	}
+
+}
